5346b6e1cd415ae28e6290a88136d3c912502cdf
Rafael Silva <deussilvao@yahoo.com>
2022-11-11 10:29:34 -0300

    Commit correto
    primeira versão do jogo com o commit correto

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
+            Console.ReadLine();
+        }
+    }
+}
